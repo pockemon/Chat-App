85bb00c92d992c275f18f650a8201908dabcf0e6
pockemon <hardikrana276@gmail.com>
2019-01-02 19:29:27 +0200

    9-firebase storing added

--- app/src/main/java/com/example/hardikrana/chatapplication/RegisterActivity.java
+++ app/src/main/java/com/example/hardikrana/chatapplication/RegisterActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,6 +32,8 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private Button mCreateButton;
+
+    private FirebaseDatabase mDatabase;
 
     //Toolbar
     private Toolbar mToolbar;
@@ -75,20 +82,41 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String email, String password) {
+    private void register_user(final String display_name, String email, String password) {
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if(task.isSuccessful()){
-                    mRegProgress.dismiss();
 
-                    Intent MainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    //When you hit back button form your android,it will not redirect you to start activity
-                    MainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(MainIntent);
-                    finish();
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
+
+                    mDatabase = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef  = mDatabase.getReference().child("Users").child(uid);
+
+                    HashMap<String,String> userMap = new HashMap<>();
+                    userMap.put("name",display_name);
+                    userMap.put("status","Hi there,I'm using chat app");
+                    userMap.put("image","default");
+                    userMap.put("thumb_image","default");
+
+                    myRef.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            mRegProgress.dismiss();
+
+                            Intent MainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            //When you hit back button form your android,it will not redirect you to start activity
+                            MainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(MainIntent);
+                            finish();
+
+
+                        }
+                    });
 
                 }
                 else
